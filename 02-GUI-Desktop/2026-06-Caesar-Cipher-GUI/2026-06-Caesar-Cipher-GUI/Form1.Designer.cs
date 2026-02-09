7a2fb6d952c7ef8625fcd78d766fa0d6fb44e0b2
TheXyberis <xyberis.work@gmail.com>
2026-02-09 23:47:03 +0100

    feat: implement Caesar Cipher desktop application using WinForms

---
+++ 02-GUI-Desktop/2026-06-Caesar-Cipher-GUI/2026-06-Caesar-Cipher-GUI/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace _2026_06_Caesar_Cipher_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label2.Location = new System.Drawing.Point(482, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Encrypted text";
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(119, 84);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(76, 22);
+            this.textBoxKey.TabIndex = 3;
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.BackColor = System.Drawing.Color.LightBlue;
+            this.buttonEncrypt.Location = new System.Drawing.Point(354, 199);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(84, 36);
+            this.buttonEncrypt.TabIndex = 7;
+            this.buttonEncrypt.Text = "Encrypt";
+            this.buttonEncrypt.UseVisualStyleBackColor = false;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackColor = System.Drawing.Color.LightBlue;
+            this.buttonSave.Location = new System.Drawing.Point(535, 369);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(180, 35);
+            this.buttonSave.TabIndex = 8;
+            this.buttonSave.Text = "Save cipher to file";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(27, 206);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(290, 198);
+            this.textBoxInput.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label1.Location = new System.Drawing.Point(22, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Enter encryption key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label3.Location = new System.Drawing.Point(22, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Enter text";
+            // 
+            // labelResult
+            // 
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResult.ForeColor = System.Drawing.Color.AliceBlue;
+            this.labelResult.Location = new System.Drawing.Point(484, 65);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Padding = new System.Windows.Forms.Padding(20);
+            this.labelResult.Size = new System.Drawing.Size(277, 281);
+            this.labelResult.TabIndex = 13;
+            this.labelResult.Paint += new System.Windows.Forms.PaintEventHandler(this.labelResult_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Encryption";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ 02-GUI-Desktop/2026-06-Caesar-Cipher-GUI/2026-06-Caesar-Cipher-GUI/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _2026_06_Caesar_Cipher_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string Encrypt(string text, int key)
+        {
+            string result = "";
+
+            foreach (char c in text)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    int charIndex = c - 'a';
+                    int newIndex = (charIndex + (key % 26) + 26) % 26;
+                    result += (char)('a' + newIndex);
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+
+            return result;
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBoxKey.Text, out int key))
+            {
+                key = 0;
+            }
+
+            labelResult.Text = Encrypt(textBoxInput.Text, key);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(labelResult.Text))
+            {
+                MessageBox.Show("Nothing to save!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "Text Files (*.txt)|*.txt|All Files(*.*)|*.*";
+            saveDialog.Title = "Save Ciphertext";
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveDialog.FileName, labelResult.Text);
+            }
+        }
+
+        private void labelResult_Paint(object sender, PaintEventArgs e)
+        {
+            using (Pen pen = new Pen(Color.FromArgb(250, 235, 215), 2))
+            {
+                int radius = 30;
+                GraphicsPath path = new GraphicsPath();
+                Rectangle rect = new Rectangle(1, 1, labelResult.Width - 3, labelResult.Height - 3);
+                path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+                path.AddArc(rect.X + rect.Width - radius, rect.Y, radius, radius, 270, 90);
+                path.AddArc(rect.X + rect.Width - radius, rect.Y + rect.Height - radius, radius, radius, 0, 90);
+                path.AddArc(rect.X, rect.Y + rect.Height - radius, radius, radius, 90, 90);
+                path.CloseAllFigures();
+
+                e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                e.Graphics.DrawPath(pen, path);
+            }
+        }
+    }
+}
