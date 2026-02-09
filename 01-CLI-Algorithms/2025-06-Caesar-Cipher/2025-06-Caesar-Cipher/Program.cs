0e8a60aaa64c0f5f6ae9f75ced12d2a87c75cdff
TheXyberis <xyberis.work@gmail.com>
2026-02-09 17:26:25 +0100

    feat: add Caesar Cipher encryption algorithm

---
+++ 01-CLI-Algorithms/2025-06-Caesar-Cipher/2025-06-Caesar-Cipher/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_06_Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Caesar Cipher ***");
+
+            Console.WriteLine("Enter lowercase text: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Enter key: ");
+            if (int.TryParse(Console.ReadLine(), out int key))
+            {
+                string encrypted = Encrypt(input, key);
+                Console.WriteLine($"Encrypted text: {encrypted}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid key.");
+            }
+        }
+        static string Encrypt(string text, int key)
+        {
+            string result = "";
+
+            foreach (char c in text)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    int charIndex = c - 'a';
+                    int newIndex = (charIndex + (key % 26) + 26) % 26;
+                    result += (char)('a' + newIndex);
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+
+            return result;
+        }
+    }
+}
