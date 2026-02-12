82a3705a3ba13520397d5fd78e84a49e598e3c1e
TheXyberis <xyberis.work@gmail.com>
2026-02-12 20:13:34 +0100

    feat: add event handlers logic for MainPage

--- 03-Mobile-Apps/2025-01-Home-Devices-Mobile/2025-01-Home-Devices-Mobile/MainPage.xaml.cs
+++ 03-Mobile-Apps/2025-01-Home-Devices-Mobile/2025-01-Home-Devices-Mobile/MainPage.xaml.cs
@@ -2,19 +2,38 @@
 {
     public partial class MainPage : ContentPage
     {
-
+        private bool _isVacuumOn = false;
         public MainPage()
         {
             InitializeComponent();
         }
-
         private void OnWashing_Clicked(object sender, EventArgs e)
         {
-
+            if (int.TryParse(WashingEntry.Text, out int programNum))
+            {
+                if (programNum >= 1 && programNum <= 12)
+                {
+                    WashingResultLabel.Text = $"Numer prania: {programNum}";
+                }
+                else
+                {
+                    WashingResultLabel.Text = "Numer prania nie podano";
+                }
+            }
         }
         private void OnVacuum_Clicked(object sender, EventArgs e)
-        { 
-
+        {
+            _isVacuumOn = !_isVacuumOn;
+            if (_isVacuumOn)
+            {
+                VacuumButton.Text = "Wyłącz";
+                VacuumStatusLabel.Text = "Odkurzac włączony";
+            }
+            else
+            {
+                VacuumButton.Text = "Włącz";
+                VacuumStatusLabel.Text = "Odkurzac wyłączony";
+            }
         }
     }
 }
